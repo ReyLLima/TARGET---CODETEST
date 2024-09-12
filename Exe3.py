61a6ad7751188465674a389ae1f47c824caa1942
ReyLLima <r.lima.l@outlook.com>
2024-09-12 20:31:50 -0300

    Exe3final

---
+++ Exe3.py
@@ -0,0 +1,151 @@
+valor_list = []
+pont = 0
+countdays = []
+
+data = [
+	{
+		"dia": 1,
+		"valor": 22174.1664
+	},
+	{
+		"dia": 2,
+		"valor": 24537.6698
+	},
+	{
+		"dia": 3,
+		"valor": 26139.6134
+	},
+	{
+		"dia": 4,
+		"valor": 0.0
+	},
+	{
+		"dia": 5,
+		"valor": 0.0
+	},
+	{
+		"dia": 6,
+		"valor": 26742.6612
+	},
+	{
+		"dia": 7,
+		"valor": 0.0
+	},
+	{
+		"dia": 8,
+		"valor": 42889.2258
+	},
+	{
+		"dia": 9,
+		"valor": 46251.174
+	},
+	{
+		"dia": 10,
+		"valor": 11191.4722
+	},
+	{
+		"dia": 11,
+		"valor": 0.0
+	},
+	{
+		"dia": 12,
+		"valor": 0.0
+	},
+	{
+		"dia": 13,
+		"valor": 3847.4823
+	},
+	{
+		"dia": 14,
+		"valor": 373.7838
+	},
+	{
+		"dia": 15,
+		"valor": 2659.7563
+	},
+	{
+		"dia": 16,
+		"valor": 48924.2448
+	},
+	{
+		"dia": 17,
+		"valor": 18419.2614
+	},
+	{
+		"dia": 18,
+		"valor": 0.0
+	},
+	{
+		"dia": 19,
+		"valor": 0.0
+	},
+	{
+		"dia": 20,
+		"valor": 35240.1826
+	},
+	{
+		"dia": 21,
+		"valor": 43829.1667
+	},
+	{
+		"dia": 22,
+		"valor": 18235.6852
+	},
+	{
+		"dia": 23,
+		"valor": 4355.0662
+	},
+	{
+		"dia": 24,
+		"valor": 13327.1025
+	},
+	{
+		"dia": 25,
+		"valor": 0.0
+	},
+	{
+		"dia": 26,
+		"valor": 0.0
+	},
+	{
+		"dia": 27,
+		"valor": 25681.8318
+	},
+	{
+		"dia": 28,
+		"valor": 1718.1221
+	},
+	{
+		"dia": 29,
+		"valor": 13220.495
+	},
+	{
+		"dia": 30,
+		"valor": 8414.61
+	}
+]
+
+for item in data:
+    if item["valor"] > 0:
+        valor_list.append(item["valor"])
+
+def menorVlist(valor_list):
+    menorvalor = min(valor_list)
+    return menorvalor
+def maiorVlist(valor_list):
+    maiorvalor = max(valor_list)
+    return maiorvalor
+def medialista(valor_list):
+    media = sum(valor_list) / len(valor_list)
+    return media
+
+while valor_list[pont] > medialista(valor_list):
+    pont += 1
+    countdays.append(valor_list[pont])
+
+def contarmaxfat(countdays):
+    quant = len(countdays)
+    return quant
+print("O menor valor de faturamento ocorrido neste mês foi de ",menorVlist(valor_list))
+print("O menor valor de faturamento ocorrido neste mês foi de  ",maiorVlist(valor_list))
+print("O número de dias neste mês que o valor de faturament diário foi superior à média mensal é de ",contarmaxfat(countdays))
